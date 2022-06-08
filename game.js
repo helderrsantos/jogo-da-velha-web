23b31022ed7e82dffb3f887581b97cfc6b193f40
Helder Rodrigo <helderr_santosd@hotmail.com>
2022-06-07 23:01:41 -0300

    Imagens ajustadas nos inputs

--- game.js
+++ game.js
@@ -1,11 +1,8 @@
 const casas = document.getElementsByTagName('input')
 const b_reiniciar = document.getElementById('reiniciar');
-const label_jogador = document.getElementById('jogador');
-const joga_um = document.getElementById('jogador1');
-const joga_dois = document.getElementById('jogador2');
 
-var jogador = '_'; //Define o jogador atual (_ = jogador indefinido; X = jogador X, O = jogador O) 
-var vencedor = '_'; //Define se há um vencedor ou não (_ = indefinido; X = jogador X, O = jogador O) 
+var jogador = '_'; //Define o jogador atual (_ = jogador indefinido; H = jogador Harry, V = jogador Voldemort) 
+var vencedor = '_'; //Define se há um vencedor ou não (_ = indefinido; H = jogador Harry, V = jogador Voldemort)
 var finish;
 
 //Define a resposta ao evento de clique nas casas do "tabuleiro"
@@ -13,10 +10,11 @@ for(var i=0;i<9;i++) {
 	casas[i].addEventListener('click', (event) => {
 		//se a casa estiver vazia e ninguém tiver vencido a partida
 		if( (event.target.value=='_') && (vencedor=='_')) {
-			event.target.value=jogador; //preenche a casa com X ou O
+			event.target.value=jogador; //preenche a casa com H ou V
 
 			event.target.style.backgroundImage= jogador === "H" ? "url('./imagens/harry.jpg')" : "url('./imagens/voldemort.jpg')";
-			event.target.style.backgroundSize='cover';
+			event.target.style.backgroundSize='100% 100%';
+			event.target.style.backgroundRepeat='no-repeat';
 			
 			trocarJogador(); //função que troca a vez do jogador, a ser definida depois
 
@@ -32,8 +30,10 @@ for(var i=0;i<9;i++) {
 b_reiniciar.addEventListener('click', (event) => {
 	for(var i=0;i<9;i++) {
 		casas[i].value='_'; //Limpa todas as casas
-		casas[i].style.color='#F7FE2E'; //Torna o valor _ invisível
-		casas[i].style.backgroundColor='#F7FE2E'; //Torna o fundo branco
+		casas[i].style.color='#ffffff00'; //Torna o valor _ invisível
+		casas[i].style.backgroundColor='#ffffff25'; //Torna o fundo branco
+		casas[i].style.backgroundImage=null;
+		casas[i].style.borderColor='#000';
 	}
 
 	vencedor = '_'; //Reseta o vencedor
@@ -44,90 +44,98 @@ b_reiniciar.addEventListener('click', (event) => {
 //Usa uma função que decide aleatoriamente o jogar a fazer a primeira jogada
 var sortearJogador = function() {
 	if(Math.floor(Math.random() * 2)==0) {
-		jogador = "H"; //define o jogador O como atual
-		label_jogador.innerHTML="Harry"; //exibe na página qual é o jogador atual
-		label_jogador.style.color='red'; //deixa o texto na cor vermelha
+		jogador = "H"; //define o jogador H como atual
+		
 	}else{
-		jogador = "V";//define o jogador X como atual
-		label_jogador.innerHTML="Voldemort"; //exibe na página qual é o jogador atual
-		label_jogador.style.color='green'; //deixa o texto na cor azul
+		jogador = "V";//define o jogador V como atual
+		
 	}
 }
 
 sortearJogador(); //Escolhe aleatoriamento o jogador inicial
 
-//Alterna a vez entre os jogadores X e Y
+//Alterna a vez entre os jogadores H e V
 var trocarJogador = function() {
 	if(jogador=='V') {
 		jogador='H';
-		label_jogador.innerHTML='H';
-		label_jogador.style.color='red';
+		
 		
 	}else{
 		jogador='V';
-		label_jogador.innerHTML='V';
-		label_jogador.style.color='green';
+		
 	}
 }
 
-//Verifica se uma condição de vitória foi atingida e colore a linha da vitória
-var vitoria = function() {
+//Verifica se uma condição de vitória foi atingida e colore a borda da vitória
+var vitoria = function () {
 	if((casas[0].value==casas[1].value) && (casas[1].value==casas[2].value) && casas[0].value!='_' ) {
-		casas[0].style.backgroundColor='#0F0';
-		casas[1].style.backgroundColor='#0F0';
-		casas[2].style.backgroundColor='#0F0';
+		casas[0].style.borderColor='#0F0';
+		casas[1].style.borderColor='#0F0';
+		casas[2].style.borderColor='#0F0';
 
 		return casas[0].value;
 
 	}else if((casas[3].value==casas[4].value) && (casas[4].value==casas[5].value) && casas[3].value!='_' ) {
-		casas[3].style.backgroundColor='#0F0';
-		casas[4].style.backgroundColor='#0F0';
-		casas[5].style.backgroundColor='#0F0';
+		casas[3].style.borderColor='#0F0';
+		casas[4].style.borderColor='#0F0';
+		casas[5].style.borderColor='#0F0';
 
 		return casas[3].value;
 
 	}else if((casas[6].value==casas[7].value) && (casas[7].value==casas[8].value) && casas[6].value!='_' ) {
-		casas[6].style.backgroundColor='#0F0';
-		casas[7].style.backgroundColor='#0F0';
-		casas[8].style.backgroundColor='#0F0';
+		casas[6].style.borderColor='#0F0';
+		casas[7].style.borderColor='#0F0';
+		casas[8].style.borderColor='#0F0';
 
 		return casas[6].value;
 
 	}else if((casas[0].value==casas[3].value) && (casas[3].value==casas[6].value) && casas[0].value!='_' ) {
-		casas[0].style.backgroundColor='#0F0';
-		casas[3].style.backgroundColor='#0F0';
-		casas[6].style.backgroundColor='#0F0';
+		casas[0].style.borderColor='#0F0';
+		casas[3].style.borderColor='#0F0';
+		casas[6].style.borderColor='#0F0';
 
 		return casas[0].value;
 
 	}else if((casas[1].value==casas[4].value) && (casas[4].value==casas[7].value) && casas[1].value!='_' ) {
-		casas[1].style.backgroundColor='#0F0';
-		casas[4].style.backgroundColor='#0F0';
-		casas[7].style.backgroundColor='#0F0';
+		casas[1].style.borderColor='#0F0';
+		casas[4].style.borderColor='#0F0';
+		casas[7].style.borderColor='#0F0';
 
 		return casas[1].value;
 
 	}else if((casas[2].value==casas[5].value) && (casas[5].value==casas[8].value) && casas[2].value!='_' ) {
-		casas[2].style.backgroundColor='#0F0';
-		casas[5].style.backgroundColor='#0F0';
-		casas[8].style.backgroundColor='#0F0';
+		casas[2].style.borderColor='#0F0';
+		casas[5].style.borderColor='#0F0';
+		casas[8].style.borderColor='#0F0';
 
 		return casas[2].value;
+
 	}else if((casas[0].value==casas[4].value) && (casas[4].value==casas[8].value) && casas[0].value!='_' ) {
-		casas[0].style.backgroundColor='#0F0';
-		casas[4].style.backgroundColor='#0F0';
-		casas[8].style.backgroundColor='#0F0';
+		casas[0].style.borderColor='#0F0';
+		casas[4].style.borderColor='#0F0';
+		casas[8].style.borderColor='#0F0';
 
 		return casas[0].value;
 
 	}else if((casas[2].value==casas[4].value) && (casas[4].value==casas[6].value) && casas[2].value!='_' ) {
-		casas[2].style.backgroundColor='#0F0';
-		casas[4].style.backgroundColor='#0F0';
-		casas[6].style.backgroundColor='#0F0';
+		casas[2].style.borderColor='#0F0';
+		casas[4].style.borderColor='#0F0';
+		casas[6].style.borderColor='#0F0';
 
 		return casas[2].value;
+		
 	}
 	
-								
     return '_';
+
+	
+
 }
+
+
+
+
+
+
+
+
